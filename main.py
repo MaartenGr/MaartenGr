8ea54662094f04ab14153a5d439972fabcb9d396
MaartenGr <maartengrootendorst@gmail.com>
2025-08-05 12:04:20 +0200

    Update articles

--- main.py
+++ main.py
@@ -11,20 +11,51 @@ from rich.tree import Tree
 
 console = Console(record=True, width=120)
 
-tree = Tree("😄 [link=https://www.maartengrootendorst.com]Maarten Grootendorst", guide_style="bold cyan")
+tree = Tree(
+    "😄 [link=https://www.maartengrootendorst.com]Maarten Grootendorst",
+    guide_style="bold cyan",
+)
 book_tree = tree.add("📖 Book Author")
-book_tree.add("[link=https://www.amazon.com/Hands-Large-Language-Models-Alammar-ebook/dp/B0DGZ46G88]Hands-On Large Language Models")
+book_tree.add(
+    "[link=https://www.amazon.com/Hands-Large-Language-Models-Alammar-ebook/dp/B0DGZ46G88]Hands-On Large Language Models"
+)
 packages_tree = tree.add("🧙‍♂️ Open Sourcerer")
 packages_tree.add("[link=https://github.com/MaartenGr/BERTopic]BERTopic")
 packages_tree.add("[link=https://github.com/MaartenGr/KeyBERT]KeyBERT")
 packages_tree.add("[link=https://github.com/MaartenGr/PolyFuzz]PolyFuzz")
 articles_tree = tree.add("📘 Demystifying AI")
-articles_tree.add("[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-mixture-of-experts]A Visual Guide to Mixture of Experts")
-articles_tree.add("[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-quantization]A Visual Guide to Quantization")
-articles_tree.add("[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-mamba-and-state]A Visual Guide to Mamba")
-articles_tree.add("[link=https://towardsdatascience.com/9-distance-measures-in-data-science-918109d069fa?sk=5a95055c23e46aed2db69271b559b464]9 Distance Measures")
-articles_tree.add("[link=https://towardsdatascience.com/keyword-extraction-with-bert-724efca412ea?sk=97a99c2669bb16f22f2f362820ba6bef]Keyword Extraction with BERT")
-articles_tree.add("[link=https://towardsdatascience.com/topic-modeling-with-bert-779f7db187e6?sk=0b5a470c006d1842ad4c8a3057063a99]Topic Modeling with BERT")
+
+articles_tree.add(
+    "[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-llm-agents]A Visual Guide to LLM Agents"
+)
+articles_tree.add(
+    "[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-reasoning-llms]A Visual Guide to Reasoning LLMs"
+)
+articles_tree.add(
+    "[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-mixture-of-experts]A Visual Guide to Mixture of Experts"
+)
+articles_tree.add(
+    "[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-quantization]A Visual Guide to Quantization"
+)
+articles_tree.add(
+    "[link=https://newsletter.maartengrootendorst.com/p/a-visual-guide-to-mamba-and-state]A Visual Guide to Mamba"
+)
+# articles_tree.add(
+#     "[link=https://towardsdatascience.com/9-distance-measures-in-data-science-918109d069fa?sk=5a95055c23e46aed2db69271b559b464]9 Distance Measures"
+# )
+# articles_tree.add(
+#     "[link=https://towardsdatascience.com/keyword-extraction-with-bert-724efca412ea?sk=97a99c2669bb16f22f2f362820ba6bef]Keyword Extraction with BERT"
+# )
+# articles_tree.add(
+#     "[link=https://towardsdatascience.com/topic-modeling-with-bert-779f7db187e6?sk=0b5a470c006d1842ad4c8a3057063a99]Topic Modeling with BERT"
+# )
+course_tree = tree.add("📖 Course Author")
+course_tree.add(
+    "[link=https://www.deeplearning.ai/short-courses/how-transformer-llms-work/]DeepLearning.AI | How Transformer LLMs Work"
+)
+course_tree.add(
+    "[link=https://www.analyticsvidhya.com/courses/building-smarter-llms-with-mamba-and-state-space-model/]Analytics Vidhya | Building Smarter LLMs with Mamba"
+)
 
 
 about = """\
